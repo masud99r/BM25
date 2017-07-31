a42c682acf70d0afd1a7a4675457c2b6e623b9c1
mr5ba <mr5ba@virginia.edu>
2017-07-31 11:27:33 -0400

    add new bm25 scoring function

--- src/query.py
+++ src/query.py
@@ -2,6 +2,7 @@ __author__ = 'Nick Hirakawa'
 
 from invdx import build_data_structures
 from rank import score_BM25
+from rank import score_BM25_formula
 import operator
 
 
@@ -19,11 +20,14 @@ class QueryProcessor:
 	def run_query(self, query):
 		query_result = dict()
 		for term in query:
+			if term == "NONE":
+				continue
 			if term in self.index:
 				doc_dict = self.index[term] # retrieve index entry # THE TRICKS
+				#print("DF of term="+str(term)+" is="+str(len(doc_dict)))
+				doc_freq = len(doc_dict)
 				for docid, freq in doc_dict.iteritems(): #for each document and its word frequency
-					score = score_BM25(n=len(doc_dict), f=freq, qf=1, r=0, N=len(self.dlt),
-									   dl=self.dlt.get_length(docid), avdl=self.dlt.get_average_length()) # calculate score
+					score = score_BM25_formula(n=len(doc_dict), f=freq, qf=1, r=0, N=len(self.dlt),dl=self.dlt.get_length(docid), avdl=self.dlt.get_average_length(), df =doc_freq) # calculate score
 					if docid in query_result: #this document has already been scored once
 						query_result[docid] += score
 					else:
--- src/rank.py
+++ src/rank.py
@@ -9,22 +9,26 @@ b = 0.75
 R = 0.0
 
 
-def score_BM25(n, f, qf, r, N, dl, avdl):
+def score_BM25(n, f, qf, r, N, dl, avdl,df):
 	K = compute_K(dl, avdl)
 	first = log( ( (r + 0.5) / (R - r + 0.5) ) / ( (n - r + 0.5) / (N - n - R + r + 0.5)) )
 	second = ((k1 + 1) * f) / (K + f)
 	third = ((k2+1) * qf) / (k2 + qf)
 	return first * second * third
-def score_BM25_formula(n, f, qf, r, N, dl, avdl):
+def score_BM25_formula(n, f, qf, r, N, dl, avdl, df):
+	# f is raw term freq, make it sublinear tf scaling
+	f = 1.0 + log(f)
 	upper_doc = (k1 + 1) * f
-	lower_doc = k1 * ((1 - b) + b * (float(dl) / float(dl))) + f
+	lower_doc = k1 * ((1 - b) + b * (float(dl) / float(avdl))) + f
 	doc_weight = (upper_doc * 1.0) / lower_doc
 
 	upper_query = (k2 + 1) * qf
 	lower_query = k2 + qf
 	query_weight = (upper_query * 1.0) / lower_query
-
-	temp_doc_score = qterm_idf * doc_weight * query_weight
+	#qterm_idf = 1.0 + log(float(N)/float(df)) # method 1
+	qterm_idf = 1.0 + log((1.0 + float(N))/(1.0 + float(df))) #smoothed idf. method 2
+	calculated_doc_score = qterm_idf * doc_weight * query_weight
+	return calculated_doc_score
 
 def compute_K(dl, avdl):
 	return k1 * ((1-b) + b * (float(dl)/float(avdl)) )
